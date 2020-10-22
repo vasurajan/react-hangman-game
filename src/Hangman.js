cd1e49edd16093fe74ebc5ac87b8c8bf68bbb83c
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-22 09:46:07 +0530

    added game over message to the screen

--- src/Hangman.js
+++ src/Hangman.js
@@ -58,13 +58,17 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
+    let gameOver = this.state.nWrong >= this.props.maxWrong;
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
         <p>Wrong Guesses: {this.state.nWrong}</p>
-        <p className='Hangman-word'>{this.guessedWord()}</p>
-        <p className='Hangman-btns'>{this.generateButtons()}</p>
+        <p className='Hangman-word'>{!gameOver ? this.guessedWord(): this.state.answer }</p>
+        <p className='this.state.nWrongHangman-btns'>
+          {!gameOver ? this.generateButtons() :
+            `You Lose`}
+        </p>
       </div>
     );
   }
