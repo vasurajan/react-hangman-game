e56cdc6db9bc89dcfcbec09e18305cff59133e49
vasurajan <63305945+vasurajan@users.noreply.github.com>
2020-10-22 09:49:51 +0530

    added alt attribute to the image tag

--- src/Hangman.js
+++ src/Hangman.js
@@ -58,11 +58,12 @@ class Hangman extends Component {
 
   /** render: render game */
   render() {
-    let gameOver = this.state.nWrong >= this.props.maxWrong;
+    const gameOver = this.state.nWrong >= this.props.maxWrong;
+    const altText = `${this.state.nWrong}/${this.props.maxWrong} Guesses`;
     return (
       <div className='Hangman'>
         <h1>Hangman</h1>
-        <img src={this.props.images[this.state.nWrong]} />
+        <img src={this.props.images[this.state.nWrong]} alt={altText} />
         <p>Wrong Guesses: {this.state.nWrong}</p>
         <p className='Hangman-word'>{!gameOver ? this.guessedWord(): this.state.answer }</p>
         <p className='this.state.nWrongHangman-btns'>
